24ea7751285c257fdd525430ebbab50e2349a1cc
unknown <loc842211@gmail.com>
2023-12-17 08:25:59 +0700

    Theming trong Material UI

--- src/App.jsx
+++ src/App.jsx
@@ -2,11 +2,13 @@
 import Button from '@mui/material/Button';
 import { pink } from '@mui/material/colors';
 import HomeIcon from '@mui/icons-material/home'
-
+import Typography from '@mui/material/Typography'
 function App() {
   return (
     <>
       <div>Quoc loc</div>
+
+      <Typography variant='body' color='text.secondary'>Typograp</Typography>
       <Button variant='text'>Text</Button>
       <Button variant='contained'>contained</Button>
       <Button variant='outlined'>outlined</Button>
