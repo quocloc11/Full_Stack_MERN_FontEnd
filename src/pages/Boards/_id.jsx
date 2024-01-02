210e01cc9ba4a73c03bc15846f38cdb590c60bf5
unknown <loc842211@gmail.com>
2024-01-02 10:17:31 +0700

    goi api va xu ly cors

--- src/pages/Boards/_id.jsx
+++ src/pages/Boards/_id.jsx
@@ -4,13 +4,26 @@ import BoarBar from './BoardBar/BoardBar'
 import AppBar from '~/components/AppBar/AppBar'
 import BoardContent from './BoardContent/BoardContent'
 import { mockData } from '~/apis/mock-data'
+import { useEffect, useState } from 'react'
+import { fetchBoardDetailsAPI } from '~/apis'
 
 function Board() {
+  const [board, setBoard] = useState(null)
+
+  useEffect(() => {
+    const boardId = '65937a164878cdc61d4d0822'
+
+    fetchBoardDetailsAPI(boardId).then((board) => {
+      setBoard(board)
+    })
+
+  }, [])
+
   return (
     <Container disableGutters maxWidth={false} sx={{ height: '100vh' }}>
       <AppBar />
-      <BoarBar board={mockData?.board} />
-      <BoardContent board={mockData?.board} />
+      <BoarBar board={board} />
+      <BoardContent board={board} />
     </Container>
   )
 }
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,1 +1,1 @@
-// export const API_ROOT="'http://localhost"
+export const API_ROOT = 'http://localhost:8017'
